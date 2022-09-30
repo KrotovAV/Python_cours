9d23500e625c9493a6cb1a6b64bee4e76f16445d
KrotovAV <KrotovAV@tut.by>
2022-09-30 17:55:32 +0300

    2_Lesson_HW2_3_2_4

--- second_lesson/second_HW/HW2_3.py
+++ second_lesson/second_HW/HW2_3.py
@@ -1,2 +1,17 @@
 # 3 Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
 
+print('')
+print('Write a namber'); 
+n = int(input())
+
+print('-------')
+list = []
+sum = 0
+for i in range(1, n+1):
+    a = (1 + 1/i)**i
+    list.append(a)
+    sum = sum + a
+
+print(list)
+print('sum =', sum)
+print()
--- second_lesson/second_HW/HW2_4.py
+++ second_lesson/second_HW/HW2_4.py
@@ -1,2 +1,34 @@
-# 4 Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+# 4 Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в 
+# файле file.txt в одной строке одно число.
 
+print('')
+print('Write a namber'); 
+n = int(input())
+
+print('-----------')
+with open('D:/GeekBrains/My Git/2 znakomstvo s iazikami/02 python/Python_cours/second_lesson/second_HW/file.txt','r') as file:
+    a = int(file.readline())
+    b = int(file.readline(2))
+
+print('a =', a)
+print('b =', b)
+
+print('-------')
+list = []
+sum = 0
+for i in range(-n, n+1):
+    list.append(i)
+
+print(list)
+# print('sum =', sum)
+print('-----------')
+
+print(list[a])
+print(list[b])
+print('------------')
+p = list[a] * list[b]
+
+print(p)
+
+print()
