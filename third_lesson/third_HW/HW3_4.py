6e34964dd2c4e036184c4754d0d3b20348bdd7c0
KrotovAV <KrotovAV@tut.by>
2022-10-04 23:34:02 +0300

    third_lessonHW3_4

--- third_lesson/third_HW/HW3_4.py
+++ third_lesson/third_HW/HW3_4.py
@@ -3,3 +3,39 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+
+print('')
+print('Write a namber'); 
+n = int(input())
+# n = 73
+print('')
+print('namber in decimal =', n)
+
+result = []
+res = ''
+while n != 0:
+    b = int(n/2)
+    c = n%2
+    n = b
+    result.append(str(c))
+
+# print(result)
+
+vrem = ''
+b = len(result)
+
+for i in range(len(result)):
+    if i < int(b/2):
+        vrem = result[b-1-i]
+        result[b-1-i] = result[i]
+        result[i] = vrem
+
+print('-----')
+# print(result)
+namber10 = 0
+
+for i in range(len(result)):
+    namber10 = namber10*10 + int(result[i])
+print('namber in binary = ', namber10)
+print()
