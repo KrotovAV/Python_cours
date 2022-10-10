466b3c2af4c58d04ef09ba600612655b4cffc598
KrotovAV <KrotovAV@tut.by>
2022-10-10 18:37:18 +0300

    third_lessonHW4_3

--- fourth_lesson/fourth_HW/HW4_3.py
+++ fourth_lesson/fourth_HW/HW4_3.py
@@ -4,3 +4,30 @@
 # 47756688399943 -> [5]
 # 1113384455229 -> [8,9]
 # 1115566773322 -> []
+
+print()
+str = '47756688399943'
+print(str)
+print('--------------')
+nambers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+list = []
+for i in range(len(str)):
+    list.append(str[i])
+
+# print(list)
+
+list2 = []
+co= 0
+com = 1
+for j in range(len(nambers)):
+    co = 0
+    for k in range(len(list)):
+        if nambers[j] == list[k]:
+            co = co + 1
+    # print(nambers[j], co)
+    if 0 < co < 2:
+        # print(com, co)
+        list2.append(nambers[j])
+
+print(list2)
+print()
